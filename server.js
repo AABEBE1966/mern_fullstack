6f0e65dc79ba8d5798e710097efcd25170061070
Abebe Amare <aabebe1966@gmail.com>
2021-12-11 08:32:54 -0800

    Added some changes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 
 let express = require('express')
 let bodyParser = require('body-parser');
-let mongoose=require("mongoose")
+let mongoose = require("mongoose")
 const dotenv = require("dotenv");
+const cors = require('cors')
 
 let app = express();
 
@@ -13,22 +14,22 @@ dotenv.config({
 let port = process.env.PORT || 5000
 
 
-let apiRoutes= require("./routes")
+let apiRoutes = require("./routes")
 
 // app.use(express.json())
 app.use(bodyParser.urlencoded({
     extended: true
- }));
- app.use(bodyParser.json());
-
+}));
+app.use(bodyParser.json());
+app.use(cors())
 app.use("/api", apiRoutes)
 
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}, (err)=>{
-    if(err) {
+}, (err) => {
+    if (err) {
         console.log(`DB is not connected. Error message ${err}`)
     } else {
         console.log("successfully connected to db")
@@ -43,9 +44,9 @@ app.listen(port, function () {
 // mongodb+srv://MERN_fullstack:pass@cluster0.35cyz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 /*
 MVC --> model, view, controller
-- create database --> 
+- create database -->
 - define database schema
 - we create accounts
-- we post contents 
-- we interact with posts --> like or comment 
+- we post contents
+- we interact with posts --> like or comment
 */
