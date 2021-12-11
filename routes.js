a2e76646e6b06085120deef1599b920f1311428c
Abebe Amare <aabebe1966@gmail.com>
2021-12-11 08:25:53 -0800

    Added some changes

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.get("/", (req, res) => {
     res.status(200).json({
-        message: "Welcome EthioBook."
+        message: "Welcome EthioBook.Welcome to the home page"
     })
 })
 
